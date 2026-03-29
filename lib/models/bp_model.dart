32bb08eabfb651870ee0033f49a4488807fa5716
nagsubhrajyoti5-hub <nagsubhrajyoti5@gmail.com>
2026-03-29 23:18:48 +0530

    Create lib/models/bp_model.dart

---
+++ lib/models/bp_model.dart
@@ -0,0 +1,11 @@
+class BPModel {
+  final int systolic;
+  final int diastolic;
+  final DateTime date;
+
+  BPModel({
+    required this.systolic,
+    required this.diastolic,
+    required this.date,
+  });
+}
